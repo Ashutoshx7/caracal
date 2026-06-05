8b54900eefd2405bdcee3e5b9306e0f0bce27450
RAWx18 <rawx18.dev@gmail.com>
2026-06-05 15:12:10 +0530

    Remove deprecated Terra Tax and Zephyr Pay mock SDKs and related files for compliance

--- examples/lynxCapital/_mock/sdk/zephyr_pay/zephyr_pay/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
-Caracal, a product of Garudex Labs
-
-Public surface of the Zephyr Pay provider SDK shim.
-"""
-from .client import ZephyrPayClient, ZephyrError, Payout
-
-__all__ = ["ZephyrPayClient", "ZephyrError", "Payout"]
