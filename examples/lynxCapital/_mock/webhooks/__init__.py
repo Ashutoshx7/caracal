e8614a08979c7f375f1df95ce41495c4a2e65490
RAWx18 <rawx18.dev@gmail.com>
2026-06-05 15:12:19 +0530

    Remove deprecated mock SDKs, streaming servers, and related files for compliance

--- examples/lynxCapital/_mock/webhooks/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
-Caracal, a product of Garudex Labs
-
-Webhook dispatcher package init.
-"""
-from _mock.webhooks.dispatcher import deliver, sign
-
-__all__ = ["deliver", "sign"]
