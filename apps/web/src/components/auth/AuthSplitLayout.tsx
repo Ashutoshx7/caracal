b1871f86ba321efa9c521f7743fa24b8a1697300
RAWx18 <rawx18.dev@gmail.com>
2026-07-01 18:40:29 +0530

    fix(web): collapse logo img to satisfy prettier lint

--- apps/web/src/components/auth/AuthSplitLayout.tsx
+++ apps/web/src/components/auth/AuthSplitLayout.tsx
@@ -29,11 +29,7 @@ export function AuthSplitLayout({
         style={{ backgroundColor: "#121016" }}
       >
         <Link to="/" className="relative z-10 flex items-center">
-          <img
-            src="/caracal_dark.png"
-            alt="Caracal"
-            className="h-auto w-56 max-w-full"
-          />
+          <img src="/caracal_dark.png" alt="Caracal" className="h-auto w-56 max-w-full" />
         </Link>
 
         <div className="relative z-10 flex items-end justify-center">
